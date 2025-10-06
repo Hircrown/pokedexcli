8f983827cbf56140e3d374731eff62a284ace790
Hircrown <221099175+Hircrown@users.noreply.github.com>
2025-10-06 16:37:20 +0200

    Added help and exit commands

--- repl.go
+++ repl.go
@@ -24,6 +24,36 @@ func startRepl(placeholder string) {
 			continue
 		}
 
-		fmt.Printf("Your command was: %s\n", words[0])
+		cmdName := words[0]
+
+		cmd, exists := getCommands()[cmdName]
+		if !exists {
+			fmt.Println("Unknown command")
+		} else {
+			if err := cmd.callback(); err != nil {
+				fmt.Errorf("Callback error: %w", err)
+			}
+		}
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Give an overview of cli commands",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
 	}
 }
